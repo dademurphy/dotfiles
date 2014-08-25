df949527160bc98bb3636e90847767c964fa5e39
Johnny Graettinger <johnny531@gmail.com>
2014-08-25 00:35:52 -0400

    Authorized key; fixing setup

--- setup.sh
+++ setup.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-ln -s screenrc ~/.screenrc
+ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-ln -s vim/vimrc ~/.vimrc
+ln -s $ROOT/screen/screenrc ~/.screenrc
 
-mkdir ~/.vim
-ln -s vim/editorconfig ~/.vim/.editorconfig
-
-mkdir ~/.vim/colors
-ln -s vim/colors/solarized.vim ~/.vim/colors/solarized.vim
-
+mkdir -p ~/.vim/colors
 mkdir -p ~/.vim/ftplugin/javascript
-ln -s vim/ftplugin/javascript/mystyle.vim ~/.vim/ftplugin/javascript/mystyle.vim
-ln -s vim/ftplugin/javascript/fmt.vim ~/.vim/ftplugin/javascript/fmt.vim
+ln -s $ROOT/vim/vimrc ~/.vimrc
+ln -s $ROOT/vim/editorconfig ~/.vim/.editorconfig
+ln -s $ROOT/vim/colors/solarized.vim ~/.vim/colors/solarized.vim
+ln -s $ROOT/vim/ftplugin/javascript/mystyle.vim ~/.vim/ftplugin/javascript/mystyle.vim
+ln -s $ROOT/vim/ftplugin/javascript/fmt.vim ~/.vim/ftplugin/javascript/fmt.vim
+
+mkdir ~/.ssh
+ln -s $ROOT/ssh/authorized_keys ~/.ssh/authorized_keys
