69765d7146f707db867dbefd0613f44dbec35f11
Johnny Graettinger <johnny531@gmail.com>
2014-08-28 23:56:29 -0400

    Fix cross-login SSH_AUTH_SOCK, update Vundle, and set git defaults

--- setup.sh
+++ setup.sh
@@ -4,6 +4,8 @@ ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 ln -s $ROOT/screen/screenrc ~/.screenrc
 
+ln -s $ROOT/bash/profile ~/.profile
+
 mkdir -p ~/.vim/colors
 mkdir -p ~/.vim/ftplugin/javascript
 ln -s $ROOT/vim/vimrc ~/.vimrc
@@ -12,5 +14,9 @@ ln -s $ROOT/vim/colors/solarized.vim ~/.vim/colors/solarized.vim
 ln -s $ROOT/vim/ftplugin/javascript/mystyle.vim ~/.vim/ftplugin/javascript/mystyle.vim
 ln -s $ROOT/vim/ftplugin/javascript/fmt.vim ~/.vim/ftplugin/javascript/fmt.vim
 
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
 mkdir ~/.ssh
 ln -s $ROOT/ssh/authorized_keys ~/.ssh/authorized_keys
+
+ln -s $ROOT/gitconfig ~/.gitconfig
