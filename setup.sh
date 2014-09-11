19f63ad15251ad4a2873e3ee741366f4f024e440
Johnny Graettinger <johnny531@gmail.com>
2014-09-10 22:38:33 -0400

    Adding vim-go, and various refactorings into lang-specific styles.

--- setup.sh
+++ setup.sh
@@ -6,13 +6,13 @@ ln -s $ROOT/screen/screenrc ~/.screenrc
 
 ln -s $ROOT/bash/profile ~/.profile
 
-mkdir -p ~/.vim/colors
-mkdir -p ~/.vim/ftplugin/javascript
+mkdir -p ~/.vim/colors ~/.vim/ftplugin/javascript ~/.vim/ftplugin/go
 ln -s $ROOT/vim/vimrc ~/.vimrc
 ln -s $ROOT/vim/editorconfig ~/.vim/.editorconfig
 ln -s $ROOT/vim/colors/solarized.vim ~/.vim/colors/solarized.vim
 ln -s $ROOT/vim/ftplugin/javascript/mystyle.vim ~/.vim/ftplugin/javascript/mystyle.vim
 ln -s $ROOT/vim/ftplugin/javascript/fmt.vim ~/.vim/ftplugin/javascript/fmt.vim
+ln -s $ROOT/vim/ftplugin/go/mystyle.vim ~/.vim/ftplugin/go/mystyle.vim
 
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
---
+++ vim/ftplugin/go/mystyle.vim
@@ -0,0 +1,10 @@
+setlocal tabstop=2
+setlocal shiftwidth=2
+
+" Re-map tag lookup to GoDef instead.
+map <C-]> :GoDef<CR>
+
+" vim-go option: Show type info automatically
+let g:go_auto_type_info = 1
+
+setlocal colorcolumn=80
